f111f90182e928d0a7c645f37ceb2ca8ee502f64
mythio <mythio.2909@gmail.com>
2019-07-12 09:03:16 +0530

    [chore]: Lazy init viewmodel in view

--- app/src/main/java/com/mythio/retrofitsample/main/MainActivity.kt
+++ app/src/main/java/com/mythio/retrofitsample/main/MainActivity.kt
@@ -2,14 +2,19 @@ package com.mythio.retrofitsample.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import com.mythio.retrofitsample.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        
     }
 }
